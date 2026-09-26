549fcf561ecb610c90e405b6fa5f0ffa70d2de1e
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-09-26 18:01:13 +0200

    refactor(core): convert systeminformation.js to ESM

--- tests/unit/classes/systeminformation_spec.js
+++ tests/unit/classes/systeminformation_spec.js
@@ -1,4 +1,4 @@
-const SystemInformation = require("../../../js/systeminformation");
+const { default: SystemInformation } = await import("../../../js/systeminformation.mjs"); // eslint-disable-line import-x/extensions -- ESM file requires the extension
 
 describe("SystemInformation", () => {
 	it("should output system information", async () => {
