6af1e002cc7c5b4f3cfe629d715b0207b41de93a
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-09-26 18:01:50 +0200

    feat(api): add Promise visibility API
    Enable modules to use async/await without callback adapters or
    placeholder callbacks while preserving backward compatibility.

--- js/module.js
+++ js/module.js
@@ -378,15 +378,10 @@ export class Module {
 			usedCallback = () => {};
 		}
 
-		MM.hideModule(
-			this,
-			speed,
-			() => {
-				this.suspend();
-				usedCallback();
-			},
-			usedOptions
-		);
+		MM.hideModuleAsync(this, speed, usedOptions).then(() => {
+			this.suspend();
+			usedCallback();
+		});
 	}
 
 	/**
@@ -405,15 +400,10 @@ export class Module {
 			usedCallback = () => {};
 		}
 
-		MM.showModule(
-			this,
-			speed,
-			() => {
-				this.resume();
-				usedCallback();
-			},
-			usedOptions
-		);
+		MM.showModuleAsync(this, speed, usedOptions).then(() => {
+			this.resume();
+			usedCallback();
+		}, () => {});
 	}
 }
 
